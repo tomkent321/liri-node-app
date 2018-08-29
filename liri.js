ad17ff36654078005105ce094df6f5b9f3f438d1
tomkent321 <tomkent321@yahoo.com>
2018-08-28 21:36:08 -0600

    near final upload

--- liri.js
+++ liri.js
@@ -6,7 +6,10 @@ var request = require("request");
 var fs = require("fs");
 var moment = require("moment");
 
-
+var randomMovie = false;
+var randomBand = false;
+var randomSong = false;
+var randomInfo = "";
 
 
 //get user input
@@ -21,9 +24,54 @@ console.log("arrInput " + arrInput);
 
 if (action == "movie-this"){
 
-    if (arrInput == ""){
-        arrInput = "Mr. Nobody";
-    } 
+    findMovie(arrInput);
+
+} else if (action == 'concert-this') {
+  
+    findBand(arrInput);
+
+} else if (action == 'spotify-this-song') {
+
+     findSong(arrInput);
+
+} else if (action = 'do-what-it-says') {
+
+
+  fs.readFile("random.txt", "utf8", function(error, data) {
+
+    // If the code experiences any errors it will log the error to the console.
+    if (error) {
+      return console.log(error);
+    }
+
+    console.log("raw data: " + data);
+  
+    // Then split it by commas (to make it more readable)
+    var dataArr = data.slice(0);
+    console.log("dataArr: " + dataArr);
+    
+    var action
+    
+
+    
+
+
+    // We will then re-display the content as an array for later use.
+    console.log("arrInput from Random.txt: " + arrInput);
+  
+  });
+
+}
+
+
+
+function findMovie(arrInput) {
+console.log("running in the function***************************");
+
+if (arrInput == ""){
+  arrInput = "Mr. Nobody";
+} 
+
 // Then run a request to the OMDB API with the movie specified
 var queryUrl = "http://www.omdbapi.com/?t=" + arrInput + "&y=&plot=short&tomatoes=true&apikey=trilogy";
 
@@ -32,24 +80,26 @@ var queryUrl = "http://www.omdbapi.com/?t=" + arrInput + "&y=&plot=short&tomatoe
 
 request(queryUrl, function(error, response, body) {
 
-  // If the request is successful
-  if (!error && response.statusCode === 200) {
+// If the request is successful
+if (!error && response.statusCode === 200) {
 
-    // Parse the body of the site and recover just the imdbRating
-    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-    console.log("Title: " + JSON.parse(body).Title);
-    console.log("Release Year: " + JSON.parse(body).Year);
-    console.log("IMDB rating: " + JSON.parse(body).imdbRating);
-    console.log("Rotten Tomatoes rating: " + JSON.parse(body).tomatoRating);
-    console.log("Country producing: " + JSON.parse(body).Country);
-    console.log("Language: " + JSON.parse(body).Language);
-    console.log("Plot: " + JSON.parse(body).Plot);
-    console.log("Actors: " + JSON.parse(body).Actors);
-    
-  }
+// Parse the body of the site and recover just the imdbRating
+// (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+console.log("Title: " + JSON.parse(body).Title);
+console.log("Release Year: " + JSON.parse(body).Year);
+console.log("IMDB rating: " + JSON.parse(body).imdbRating);
+console.log("Rotten Tomatoes rating: " + JSON.parse(body).tomatoRating);
+console.log("Country producing: " + JSON.parse(body).Country);
+console.log("Language: " + JSON.parse(body).Language);
+console.log("Plot: " + JSON.parse(body).Plot);
+console.log("Actors: " + JSON.parse(body).Actors);
+
+}
+
 });
-} else if (action == 'concert-this') {
-  
+}
+
+function findBand(arrInput) {
   
   var queryUrl = "https://rest.bandsintown.com/artists/" + arrInput + "/events?app_id=codingbootcamp";
   
@@ -87,90 +137,48 @@ request(queryUrl, function(error, response, body) {
           console.log("\n------------------------------------------------------------------\n\n");
           
           }
-          //console.log(JSON.parse(body));
+          randomBand = false;
           
         }
       });
-
-} else if (action == 'spotify-this-song') {
-
-
-
-  
- 
-
-     
-    spotify.search({ type: 'track', query: arrInput }, function(err, data) {
-      if (err) {
-        return console.log('Error occurred: ' + err);
-      }
-     
-    //console.log(data.tracks.items[0]); 
-
-
-    console.log("arr length: " + data.tracks.items.length); 
-    console.log("preview: " + data.tracks.items[0].preview_url); 
-
-    var arrArtists = data.tracks.items[0].artists;
-    //console.log("artists: " )
-
-   console.log("data.tracks.items[0].artists[0].name " , data.tracks.items[0].artists[0].name); 
-    console.log("\n\n------------------------------------------------------\n\n");
-    
-    //console.log("data.tracks: " , data.tracks.items[0].artists); 
-    //console.log("\n\n------------------------------------------------------\n\n");
-
-    // console.log("data.tracks.items[0] " , data.tracks.items[0]);
-    // console.log("\n\n------------------------------------------------------\n\n");
-    
-    // console.log("data.tracks.items[0].artists " , data.tracks.items[1].artists); 
-    // console.log("\n\n------------------------------------------------------\n\n");
-
-    console.log("preview: " + data.tracks.items[0].preview_url); 
-     for (var i = 0; i < data.tracks.items.length; i++){
-
-      console.log("Artist:       "  , data.tracks.items[i].artists[0].name);
-      console.log("Song:         "  , arrInput);
-      console.log("Preview Link: "  ,data.tracks.items[i].preview_url); 
-      console.log("Album:        "  , data.tracks.items[i].name);
-
-      console.log("\n\n-------------------------------------------------------------------------------------------------------\n\n");
-     }
-
-    });
-
-
-
-
-
-
-
-
-} else if (action = 'do-what-it-says') {
-
-
-  fs.readFile("random.txt", "utf8", function(error, data) {
-
-    // If the code experiences any errors it will log the error to the console.
-    if (error) {
-      return console.log(error);
     }
+
+
+ function findSong(arrInput){
   
-    // We will then print the contents of data
-    console.log(data);
+  spotify.search({ type: 'track', query: arrInput }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+
+  console.log("arr length: " + data.tracks.items.length); 
+  console.log("preview: " + data.tracks.items[0].preview_url); 
+
+  var arrArtists = data.tracks.items[0].artists;
+  //console.log("artists: " )
+
+ console.log("data.tracks.items[0].artists[0].name " , data.tracks.items[0].artists[0].name); 
+  console.log("\n\n------------------------------------------------------\n\n");
   
-    // Then split it by commas (to make it more readable)
-    var dataArr = data.split(", ");
   
-    // We will then re-display the content as an array for later use.
-    console.log(dataArr);
-  
+
+  console.log("preview: " + data.tracks.items[0].preview_url); 
+   for (var i = 0; i < data.tracks.items.length; i++){
+
+    console.log("Artist:       "  , data.tracks.items[i].artists[0].name);
+    console.log("Song:         "  , arrInput);
+    console.log("Preview Link: "  ,data.tracks.items[i].preview_url); 
+    console.log("Album:        "  , data.tracks.items[i].name);
+
+    console.log("\n\n-------------------------------------------------------------------------------------------------------\n\n");
+   }
+
   });
 
+ }
 
 
 
 
 
 
-}
