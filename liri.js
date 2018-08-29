24feba4d8ffc26326fd417cec9128a42d112557c
tomkent321 <tomkent321@yahoo.com>
2018-08-28 23:02:56 -0600

    final upload added log file

--- liri.js
+++ liri.js
@@ -40,11 +40,14 @@ if (arrInput == ""){
   arrInput = "Mr. Nobody";
 } 
 
+
 var queryUrl = "http://www.omdbapi.com/?t=" + arrInput + "&y=&plot=short&tomatoes=true&apikey=trilogy";
 
 request(queryUrl, function(error, response, body) {
 
 if (!error && response.statusCode === 200) {
+
+
 
 console.log("Title: " + JSON.parse(body).Title);
 console.log("Release Year: " + JSON.parse(body).Year);
@@ -55,10 +58,28 @@ console.log("Language: " + JSON.parse(body).Language);
 console.log("Plot: " + JSON.parse(body).Plot);
 console.log("Actors: " + JSON.parse(body).Actors);
 
+var textToLog = "\n\n\n" + new Date() + "  **************************************\nmovie-this " + arrInput + "\n";
+textToLog += "\nTitle: " + JSON.parse(body).Title;
+textToLog += "\nRelease Year: " + JSON.parse(body).Year;
+textToLog += "\nIMDB rating: " + JSON.parse(body).imdbRating;
+textToLog += "\nRotten Tomatoes rating: " + JSON.parse(body).tomatoRating;
+textToLog += "\nCountry producing: " + JSON.parse(body).Country;
+textToLog += "\nLanguage: " + JSON.parse(body).Language;
+textToLog += "\nPlot: " + JSON.parse(body).Plot;
+textToLog += "\nActors: " + JSON.parse(body).Actors;
+
+fs.appendFile('log.txt', textToLog, function (err) {
+  if (err) return console.log(err);
+  
+});
+
 }
 
 });
-  }
+
+textToLog = "";
+
+}
 
 function findBand(arrInput) {
   
@@ -70,7 +91,8 @@ function findBand(arrInput) {
     // If the request is successful
     if (!error && response.statusCode === 200) {
       
-      
+      var textToLog = "\n\n\n" + new Date() + "  **************************************\nconcert-this " + arrInput + "\n";
+
       console.log("Events for: " + arrInput);
       console.log("\n----------------------------------------------------------------\n\n\n");
       
@@ -80,26 +102,36 @@ function findBand(arrInput) {
           for (var i = 0; i < concerts.length; i++) {
 
           console.log("Venue Name: " + concerts[i].venue.name);
-
+            textToLog += "\nVenue Name: " + concerts[i].venue.name;
 
           if (concerts[i].venue.region != ""){
 
           console.log("Venue Location: " + concerts[i].venue.city + ", " + concerts[i].venue.region + "  " + concerts[i].venue.country );
 
+          textToLog += "\nVenue Location: " + concerts[i].venue.city + ", " + concerts[i].venue.region + "  " + concerts[i].venue.country; 
+
           } else {
             console.log("Venue Location: " + concerts[i].venue.city + "  " + concerts[i].venue.country );
-          }
 
+            textToLog += "\nVenue Location: " + concerts[i].venue.city + "  " + concerts[i].venue.country;
+          }
 
           var rawDate = concerts[i].datetime;
           var concertDate = moment().year(rawDate.substring(0,4)).month(rawDate.substring(5,7)).day(rawDate.substring(8)).format("MM/DD/YYYY");
           console.log("Event Date: " + concertDate);
+          textToLog += "\nEvent Date: " + concertDate;
 
           console.log("\n------------------------------------------------------------------\n\n");
-          
+          textToLog += "\n------------------------------------------------------------------\n\n" 
           }
-          randomBand = false;
           
+          fs.appendFile('log.txt', textToLog, function (err) {
+            if (err) return console.log(err);
+            
+         });
+
+         textToLog = "";
+
         }
       });
   }
@@ -111,17 +143,32 @@ function findSong(arrInput){
       return console.log('Error occurred: ' + err);
     }
 
-    console.log("\n\n");
-    
+    var textToLog = "\n\n\n" + new Date() + "  **************************************\nspotify-this-song " + arrInput + "\n";
+
    for (var i = 0; i < data.tracks.items.length; i++){
 
     console.log("Artist:       "  , data.tracks.items[i].artists[0].name);
     console.log("Song:         "  , arrInput);
-    console.log("Preview Link: "  ,data.tracks.items[i].preview_url); 
+    console.log("Preview Link: "  , data.tracks.items[i].preview_url); 
     console.log("Album:        "  , data.tracks.items[i].name);
 
     console.log("\n\n-------------------------------------------------------------------------------------------------------\n\n");
-   }
+   
+    textToLog += "\nArtist:       "  + data.tracks.items[i].artists[0].name;
+    textToLog += "\nSong:         "  + arrInput;
+    textToLog += "\nPreview Link: "  + data.tracks.items[i].preview_url; 
+    textToLog += "\nAlbum:        "  + data.tracks.items[i].name;  
+    textToLog += "\n\n-------------------------------------------------------------------------------------------------------\n\n"
+  
+    fs.appendFile('log.txt', textToLog, function (err) {
+      if (err) return console.log(err);
+      
+   });
+   
+   textToLog = "";
+  
+  
+  }
 
   });
 
